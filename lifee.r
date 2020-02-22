ffc0f2156b277ea5df9de495cd6fe7718240a64f
Melissa Smith <ms429@cornell.edu>
2020-02-22 18:50:56 -0500

    worked on means by sex

--- lifee.r
+++ lifee.r
@@ -15,7 +15,22 @@
 
 adata <- read.csv("~/Work/R Practice/CSP Workshop 2020/CSP2020_workflow_project/zooAnimal_medianLifeExpectancy_Jul2018 .csv")
 
+#   1) Count number of animals in each taxa
+table(adata$TaxonClass)
+
+#   2) Find average lifespan for each species
+means.species <- tapply(adata$Overall.MLE, adata$Species.Common.Name, mean, na.rm=TRUE)
+means.species
+
+#   3) Find average lifespan for each species, male and female separately
 means.taxa <- tapply(adata$Overall.MLE, adata$TaxonClass, mean, na.rm=TRUE)
 means.taxa
+
+#   4) Find species that has larges male/female diff
+boxplot(adata$Overall.MLE ~ adata$TaxonClass)
+
+means.taxa.m <- tapply(adata$Male.MLE, adata$TaxonClass, na.rm=TRUE)
+means.taxa.m
+
 table(adata$TaxonClass)
 
